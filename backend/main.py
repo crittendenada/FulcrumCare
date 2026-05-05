9464269622c086cfe5d88faf11cfa956b554b399
crittendenada <adam.crittenden@yale.edu>
2026-05-04 21:51:04 -0400

    Open CORS for demo deployment

--- backend/main.py
+++ backend/main.py
@@ -14,14 +14,9 @@ from utils.helpers import ensure_seeded
 
 app = FastAPI(title="FulcrumCare API", version="0.1.0")
 
-_cors_origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
-if os.environ.get("CORS_ORIGIN"):
-    _cors_origins.append(os.environ["CORS_ORIGIN"])
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=_cors_origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
